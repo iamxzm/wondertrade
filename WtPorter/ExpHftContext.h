4ef81556213cf88f5f21ad6aca843b63cf3605c2
志民 熊 <2422279024@qq.com>
2021-11-22 17:40:44 +0800

    向本地仓库提交WtPorter文件夹及文件夹内文件

---
+++ WtPorter/ExpHftContext.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "../WtCore/HftStraBaseCtx.h"
+
+USING_NS_OTP;
+
+class ExpHftContext : public HftStraBaseCtx
+{
+public:
+	ExpHftContext(WtHftEngine* engine, const char* name, bool bAgent):HftStraBaseCtx(engine, name, bAgent){}
+	virtual ~ExpHftContext(){}
+
+public:
+	virtual void on_bar(const char* code, const char* period, uint32_t times, WTSBarStruct* newBar) override;
+
+	virtual void on_channel_lost() override;
+
+	virtual void on_channel_ready() override;
+
+	virtual void on_entrust(uint32_t localid, const char* stdCode, bool bSuccess, const char* message) override;
+
+	virtual void on_init() override;
+
+	virtual void on_session_begin(uint32_t uTDate) override;
+
+	virtual void on_session_end(uint32_t uTDate) override;
+
+	virtual void on_order(uint32_t localid, const char* stdCode, bool isBuy, double totalQty, double leftQty, double price, bool isCanceled) override;
+
+	virtual void on_tick(const char* code, WTSTickData* newTick) override;
+
+	virtual void on_order_queue(const char* stdCode, WTSOrdQueData* newOrdQue) override;
+
+	virtual void on_order_detail(const char* stdCode, WTSOrdDtlData* newOrdDtl) override;
+
+	virtual void on_transaction(const char* stdCode, WTSTransData* newTrans) override;
+
+	virtual void on_trade(uint32_t localid, const char* stdCode, bool isBuy, double vol, double price) override;
+};
+
---
+++ WtPorter/ExpSelContext.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "../WtCore/SelStraBaseCtx.h"
+
+USING_NS_OTP;
+
+class ExpSelContext : public SelStraBaseCtx
+{
+public:
+	ExpSelContext(WtSelEngine* env, const char* name);
+	virtual ~ExpSelContext();
+
+public:
+	virtual void on_init() override;
+
+	virtual void on_session_begin(uint32_t uDate) override;
+
+	virtual void on_session_end(uint32_t uDate) override;
+
+	virtual void on_strategy_schedule(uint32_t curDate, uint32_t curTime) override;
+
+	virtual void on_bar_close(const char* stdCode, const char* period, WTSBarStruct* newBar) override;
+
+	virtual void on_tick_updated(const char* stdCode, WTSTickData* newTick) override;
+
+};
+
